1d0dc6accd59afe4fb9dfb9eec7488ecebe5c012
vadniks <whatever@fake.some>
2024-03-24 22:09:50 +0300

    License

--- demo/src/main.c
+++ demo/src/main.c
@@ -1,3 +1,11 @@
+/*
+  Arnamsneat - A SDL based GUI toolkit.
+  Copyright (C) 2024 Vadim Nikolaev (https://github.com/vadniks).
+
+  This Source Code Form is subject to the terms of the Mozilla Public
+  License, v. 2.0. If a copy of the MPL was not distributed with this
+  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
 
 #include <stdlib.h>
 #include <arnamsneat.h>
