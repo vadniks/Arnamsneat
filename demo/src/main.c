4c0e5fb81f97d6733f4679c920bef5aab3223920
vadniks <whatever@fake.some>
2024-03-24 23:21:55 +0300

    Structure & logic

--- demo/src/main.c
+++ demo/src/main.c
@@ -7,12 +7,14 @@ int main(void) {
     if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS | SDL_INIT_TIMER) != 0)
         abort();
 
+    const int scale = 5;
+
     SDL_Window* window = SDL_CreateWindow(
         "Title",
         SDL_WINDOWPOS_CENTERED,
         SDL_WINDOWPOS_CENTERED,
-        160 * 5,
-        90 * 5,
+        160 * scale,
+        90 * scale,
         SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE
     );
 
