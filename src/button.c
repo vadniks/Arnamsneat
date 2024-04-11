b05ebf0be2a215e19368538513746cc7bfa1f179
vadniks <whatever@fake.some>
2024-04-11 16:53:22 +0300

    Structure & logic

--- src/button.c
+++ src/button.c
@@ -11,6 +11,7 @@ void amstGetButtonMetrics(
     int32_t* AMST_NONNULL width,
     int32_t* AMST_NONNULL height
 ) {
+    defsAssert(gInitialized);
     *width = textWidth + 10;
     *height = textHeight + 10;
 }
@@ -23,14 +24,13 @@ void amstButton(
     void (* AMST_NONNULL clickHandler)(void)
 ) {
     defsAssert(gInitialized);
-    defsAssert(x >= 5 && y >= 5);
 
     int32_t textWidth, textHeight;
     amstGetTextMetrics(context, text, &textWidth, &textHeight);
 
     const bool mouseHovered =
-        context->mouseX >= x - 5 && context->mouseX <= x + textWidth + 10 &&
-        context->mouseY >= y - 5 && context->mouseY <= y + textHeight + 10;
+        context->mouseX >= x && context->mouseX <= x + textWidth + 10 &&
+        context->mouseY >= y && context->mouseY <= y + textHeight + 10;
 
     const bool mouseClicked = mouseHovered && context->mouseDown;
 
@@ -41,12 +41,12 @@ void amstButton(
             ? ((SDL_Color) {127, 127, 127, 127})
             : ((SDL_Color) {255, 255, 255, 255});
 
-    amstText(context, text, color, x, y);
+    amstText(context, text, color, x + 5, y + 5);
 
     uint8_t r, g, b, a;
     SDL_GetRenderDrawColor(context->renderer, &r, &g, &b, &a);
     SDL_SetRenderDrawColor(context->renderer, color.r, color.g, color.b, color.a);
-    SDL_RenderDrawRect(context->renderer, &((SDL_Rect) {x - 5, y - 5, textWidth + 10, textHeight + 10}));
+    SDL_RenderDrawRect(context->renderer, &((SDL_Rect) {x, y, textWidth + 10, textHeight + 10}));
     SDL_SetRenderDrawColor(context->renderer, r, g, b, a);
 
     if (mouseClicked) {
