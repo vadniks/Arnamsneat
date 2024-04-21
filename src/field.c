08fe2ca1120bfc2c974dd0e61c9372152207cf95
vadniks <whatever@fake.some>
2024-04-21 15:34:00 +0300

    Utf8 support

--- src/field.c
+++ src/field.c
@@ -64,103 +64,88 @@ void amstField(
     int32_t labelWidth, labelHeight;
     amstGetTextMetrics(context, label, &labelWidth, &labelHeight);
 
-    if (context->keyboardInputting) {
-        context->keyboardInputting = false;
-        state->glyphs = defsRealloc(state->glyphs, ++(state->length) * sizeof(int32_t));
-        defsAssert(state->glyphs != nullptr);
-        state->glyphs[state->length - 1] = context->nextGlyph;
-    } else if (context->keyboardInputErasing && state->length > 0) {
-        context->keyboardInputErasing = false;
-        state->length--;
-        // no realloc 'cause it's freed afterward anyway
-    }
-
-    int32_t totalWidth = 0;
-    for (int32_t i = 0; i < state->length; i++) {
-        const char* glyph = (char*) &(state->glyphs[i]);
-        SDL_Log("%s", glyph);
-
-        int32_t w, h;
-        amstGetTextMetrics(context, glyph, &w, &h);
-        totalWidth += w;
-
-        amstText(context, glyph, amstForegroundColor, x + 5 + totalWidth, y + 5);
-    }
-    SDL_Log("");
 
 
-
-//    int32_t textWidth, textHeight;
-//    if (state->glyphs != nullptr)
+    int32_t textWidth;
+    if (state->glyphs != nullptr) {
 //        amstGetTextMetrics(context, state->input, &textWidth, &textHeight);
-//    else {
-//        textWidth = 0;
-//        textHeight = 0;
-//    }
-//
-//    const int32_t
-//        width = defsMax(probeWidth, labelWidth),
-//        height = probeHeight;
-//
-//    const bool mouseHovered =
-//        context->mouseX >= x && context->mouseX <= x + width + 10 &&
-//        context->mouseY >= y && context->mouseY <= y + height + 10;
-//
-//    const bool active = context->activeField == state;
-//
-//    if (mouseHovered && context->mouseDown) {
-//        context->mouseDown = false;
-//        context->activeField = state;
-//    }
-//
-//    if (active) {
-//        if (context->keyboardInputErasing && state->length > 0) {
-//            context->keyboardInputErasing = false;
-//            state->length--;
-//            state->input[state->length] = 0;
-//            // no realloc to shrink the memory region's size 'cause it'll get freed anyway afterward
-//        }
-//
-//        if (context->keyboardInputting && textWidth < width) {
-//            context->keyboardInputting = false;
-//
-//            state->input = defsRealloc(state->input, state->length + 2);
-//            state->input[state->length] = (context->keyboardInput + context->keyboardInputSize - 1)[0];
-//            state->input[state->length + 1] = 0;
-//            state->length++;
-//
-//            inputHandler(state->input);
-//        }
-//    }
-//
-//    if (state->input != nullptr && state->length > 0)
+        textWidth = 0;
+        for (int32_t i = 0; i < state->length; i++) {
+            int32_t w, h;
+            amstGetTextMetrics(context, (char*) &(state->glyphs[i]), &w, &h);
+            textWidth += w;
+        }
+    } else
+        textWidth = 0;
+
+    const int32_t
+        width = defsMax(probeWidth, labelWidth),
+        height = probeHeight;
+
+    const bool mouseHovered =
+        context->mouseX >= x && context->mouseX <= x + width + 10 &&
+        context->mouseY >= y && context->mouseY <= y + height + 10;
+
+    const bool active = context->activeField == state;
+
+    if (mouseHovered && context->mouseDown) {
+        context->mouseDown = false;
+        context->activeField = state;
+    }
+
+    if (active) {
+        if (context->keyboardInputErasing && state->length > 0) {
+            context->keyboardInputErasing = false;
+            state->length--;
+            // no realloc to shrink the memory region's size 'cause it'll get freed anyway afterward
+        }
+
+        if (context->keyboardInputting && textWidth < width) {
+            context->keyboardInputting = false;
+            state->glyphs = defsRealloc(state->glyphs, ++(state->length) * sizeof(int32_t));
+            defsAssert(state->glyphs != nullptr);
+            state->glyphs[state->length - 1] = context->nextGlyph;
+        }
+    }
+
+    if (state->length > 0) {
 //        amstText(context, state->input, amstForegroundColor, x + 5, y + 5);
-//    else
-//        amstText(
-//            context,
-//            label,
-//            mouseHovered ? amstHoverColor : amstSecondaryColor,
-//            x + 5,
-//            y + 5
-//        );
-//
-//    SDL_Color color = amstMakeColor(
-//        mouseHovered
-//            ? amstHoverColor
-//            : active
-//                ? amstActiveColor
-//                : amstForegroundColor
-//    );
-//
-//    uint8_t r, g, b, a;
-//    SDL_GetRenderDrawColor(context->renderer, &r, &g, &b, &a);
-//    SDL_SetRenderDrawColor(context->renderer, color.r, color.g, color.b, color.a);
-//
-//    SDL_RenderDrawLine(context->renderer, x, y + height + 10, x + width + 10, y + height + 10);
-//    SDL_SetRenderDrawColor(context->renderer, r, g, b, a);
-//
-//    context->lastDrawnWidth = width + 10;
-//    context->lastDrawnHeight = height + 10;
+        int32_t totalWidth = 0;
+        for (int32_t i = 0; i < state->length; i++) {
+            const char* glyph = (char*) &(state->glyphs[i]);
+
+            int32_t w, h;
+            amstGetTextMetrics(context, glyph, &w, &h);
+            totalWidth += w;
+
+            amstText(context, glyph, amstForegroundColor, x + 5 + totalWidth, y + 5);
+        }
+    } else
+        amstText(
+            context,
+            label,
+            mouseHovered ? amstHoverColor : amstSecondaryColor,
+            x + 5,
+            y + 5
+        );
+
+    SDL_Color color = amstMakeColor(
+        mouseHovered
+            ? amstHoverColor
+            : active
+                ? amstActiveColor
+                : amstForegroundColor
+    );
+
+    uint8_t r, g, b, a;
+    SDL_GetRenderDrawColor(context->renderer, &r, &g, &b, &a);
+    SDL_SetRenderDrawColor(context->renderer, color.r, color.g, color.b, color.a);
+
+    SDL_RenderDrawLine(context->renderer, x, y + height + 10, x + width + 10, y + height + 10);
+    SDL_SetRenderDrawColor(context->renderer, r, g, b, a);
+
+    context->lastDrawnWidth = width + 10;
+    context->lastDrawnHeight = height + 10;
 }
 
 const char* AMST_NULLABLE amstFieldText(AmstFieldState* AMST_NONNULL state) {
